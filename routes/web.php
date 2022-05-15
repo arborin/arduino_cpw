d6fc26eb33dfaec5ddda99f44e00fe9e90a1af07
arborin <nika.kobaidze@gmail.com>
2022-05-15 17:54:53 +0400

    [ADD] export and chart routes

--- routes/web.php
+++ routes/web.php
@@ -45,28 +45,40 @@ Route::controller(DashboardController::class)->group(function (){
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/user-list',            [UserController::class, 'userList'])->name('user.list');
-    Route::get('/user-form/{id?}',      [UserController::class, 'userForm'])->name('user.form');
-    Route::post('/user-delete/',        [UserController::class, 'userDelete'])->name('user.delete');
-    Route::post('/user-save/',          [UserController::class, 'userSave'])->name('user.save');
+    Route::get('/user-list',            [UserController::class, 'userList'] )->name('user.list');
+    Route::get('/user-form/{id?}',      [UserController::class, 'userForm'] )->name('user.form');
+    Route::post('/user-delete/',        [UserController::class, 'userDelete'] )->name('user.delete');
+    Route::post('/user-save/',          [UserController::class, 'userSave'] )->name('user.save');
 
     Route::get('/arduino-list',         [ArduinoController::class, 'index'] )->name('arduino.list');
     Route::get('/arduino-form/{id?}',   [ArduinoController::class, 'viewArduino'] )->name('arduino.form');
     Route::post('/arduino-save',        [ArduinoController::class, 'saveArduino'] )->name('arduino.save');
     Route::post('/arduino-delete',      [ArduinoController::class, 'deleteArduino'] )->name('arduino.delete');
 
+
+
     Route::get('/button-list',          [ButtonController::class, 'buttonStatus'] )->name('button.list');
     Route::get('/button-form/{id?}',    [ButtonController::class, 'buttonStatusForm'] )->name('button.form');
     Route::post('/button-save',         [ButtonController::class, 'buttonStatusSave'] )->name('buttonAction.save');
     Route::post('/button-delete',       [ButtonController::class, 'buttonDelete'] )->name('button.delete');
 
-    Route::get('/dashboard',            [DashboardController::class, 'dashboard'])->name('dashboard');
-    Route::get('/dashboard/live',       [DashboardController::class, 'dashboardLive'])->name('dashboard.live');
+    // AJAX
+    Route::get('/button-data',          [ButtonLogController::class, 'buttonDailyData'] )->name('button_daily.data');
+    Route::get('/presure-data',         [PresureController::class, 'presureDailyData'] )->name('presure_daily.data');
+
+    Route::get('/dashboard',            [DashboardController::class, 'dashboard'] )->name('dashboard');
+    Route::get('/dashboard/live',       [DashboardController::class, 'dashboardLive'] )->name('dashboard.live');
+
+    Route::get('/button-log/{name}',            [ButtonLogController::class, 'showButtonLog'])->name('showButton.log');
+    Route::get('/presure-log/{name}',           [PresureController::class, 'showPresureLog'])->name('showPresure.log');
+
+    Route::get('/button-log/{arduino_name}',    [ButtonLogController::class, 'buttonLog'])->name('button.log');
+
+    // EXPORT DATA
+    Route::post('/button-log-export',           [ButtonLogController::class, 'exportButtonLogs'])->name('export_button.log');
+    Route::post('/presure-log-export',          [PresureController::class, 'exportPresureLogs'])->name('export_presure.log');
 });
 
 
 
-Route::get('/button-log/{name}',        [ButtonLogController::class, 'showButtonLog'])->name('showButton.log');
-Route::get('/presure-log/{name}',       [PresureController::class, 'showPresureLog'])->name('showPresure.log');
 
-Route::get('/button-log/{arduino_name}', [ButtonLogController::class, 'buttonLog'])->name('button.log');
